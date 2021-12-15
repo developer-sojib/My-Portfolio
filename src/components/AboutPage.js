5d8db9a58b1fcf3513cb97a88bf46403c2c9d76d
sojjib <developersojib.01@gmail.com>
2021-12-15 21:01:11 +0600

    final

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -67,8 +67,9 @@ const AboutPage = () => {
           <img src={astronaut} alt="spaceman" />
         </Spaceman>
         <Main>
-          I'm a front-end developer located in Sylhet, Bangladesh. I love to
-          create simple yet beautiful websites with great user experience.
+          I'm Abdul Awal Sojib, a front-end developer located in Sylhet,
+          Bangladesh. I love to create simple yet beautiful websites with great
+          user experience.
           <br /> <br />
           I'm interested in the whole frontend stack Like trying new things and
           building great projects. I'm an independent freelancer and blogger. I
